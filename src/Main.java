da30ba1d6877238b352c2c64f910596e4c651c15
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-16 20:46:38 -0400

    java Practce

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Write a Java program to print the sum of two numbers from user input.
+
+
+        Scanner sc= new Scanner(System.in);
+
+        int sum=0;
+
+        System.out.println("Enter the first number");
+        int a= sc.nextInt();
+        System.out.println("Enter the Second  number");
+        int b= sc.nextInt();
+
+        sum=a+b;
+
+
+        System.out.println("The sum of given number is "+ sum);
+
+
+
+
+
     }
 }
